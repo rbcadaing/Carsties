75cb49b77514e60cc66718a517c52cdfadc9c683
rbcadaing@gmail.com <rbcadaing@gmail.com>
2025-04-01 23:53:49 +0800

    fix the hydration issue in chrome

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <ToasterProvider />
         <Navbar />
         <main className="container mx-auto px-5 pt-10">
